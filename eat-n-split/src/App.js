355e3c44d6585c4af5c28e2a453b3be03c9ce4af
qeta fetviashvili <qetafetviashvili123@gmail.com>
2025-12-01 20:19:48 +0400

    project layout finish

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -24,7 +24,10 @@ function App() {
     <div className="app">
       <div className="sidebar">
       <FriendsList />
+      <FormAddFriend />
+      <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -44,6 +47,54 @@ function Friend({friend}) {
     <li>
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && <p className="red">You owe {friend.name} {Math.abs(friend.balance)}$</p>}
+      {friend.balance > 0 && <p className="green">{friend.name} owes you {Math.abs(friend.balance)}$</p>}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <Button>Select</Button>
     </li>
+  )
+}
+
+function Button({children}) {
+  return <button className="button">{children}</button>
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+    <label>👫 Friend name</label>
+    <input type="text" />
+
+    <label>🌆 Image URL</label>
+    <input type="text" />
+
+    <Button>Add</Button>
+    </form>
+  )
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🧍‍♀️ Your expense</label>
+      <input type="text" />
+
+      <label>👫 X's expense</label>
+      <input type="text" disabled/>
+
+      <label>🤑 who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
   )
 }
